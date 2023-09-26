6dc75c8aab8d5d2d65615b4b0975d743dbe08d20
Hussain Afzal <110721227+chhussainafzal@users.noreply.github.com>
2023-09-26 23:58:05 +0500

    Add files via upload

---
+++ bubblesort/bubblesort/bubblesort/Sort.h
@@ -0,0 +1,13 @@
+#pragma once
+class Sort
+{
+private:
+	int arr[100];
+
+public:
+	Sort();
+	void PrintArray();
+	void BubbleSort();
+	void SortedArray();
+
+};
---
+++ bubblesort/bubblesort/bubblesort/bubblesort.cpp
@@ -0,0 +1,24 @@
+// bubblesort.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include"Sort.h"
+#include<iostream>
+	using namespace std;
+
+	int main()
+	{
+
+		std::cout << "Hello World!\n";
+		
+		Sort xyz;
+		xyz.PrintArray();
+		xyz.SortedArray();
+		return 0;
+
+	}
+
+
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
